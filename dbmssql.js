2149e42a07a4d6339c00e8a0b24f9039e4fb48fe
Raja Raman <raja@godbtech.com>
2017-11-08 06:44:43 +0530

    wip

---
+++ dbmssql.js
@@ -0,0 +1,25 @@
+/* jshint node: true */
+
+/*
+SELECT *
+FROM Northwind.INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = N'Customers'
+
+ ORDINAL_POSITION, TABLE_NAME, COLUMN_NAME, COLUMN_DEFAULT, IS_NULLABLE, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, CHARACTER_OCTET_LENGTH, DATETIME_PRECISION 
+date 	datetimeoffset
+datetime2 	smalldatetime
+datetime 	time
+
+char 	varchar
+text
+
+nchar 	nvarchar
+ntext
+
+bigint 	numeric
+bit 	smallint
+decimal 	smallmoney
+int 	tinyint
+money
+
+*/
